15a80bcfd8000408a59adeeda1f278c9cf2a4414
richard-srnsw <richard.lehane@records.nsw.gov.au>
2011-06-30 17:02:47 +1000

    playing with pages

--- lib/srnsw.rb
+++ lib/srnsw.rb
@@ -7,9 +7,9 @@ module SRNSW
     include ActiveModel::Serializers::JSON
     include ActiveModel::Serializers::Xml
   
-    attr_accessor :total_results, :this_page, :offset, :per_page, :next_page, :prev_page, :total_pages
+    attr_accessor :total_results, :this_page, :offset, :per_page, :next_page, :prev_page, :total_pages, :page_param
   
-    def initialize(page, count, length)
+    def initialize(page, count, length, page_param=:page)
       @total_results = length
       @this_page = page
       @offset = (page - 1) * count
@@ -17,6 +17,7 @@ module SRNSW
       @next_page = page * count < length ? page + 1 : nil
       @prev_page = page > 1 ? page - 1 : nil
       @total_pages = (length.to_f / count).ceil
+      @page_param = page_param
     end
      
     def attributes
@@ -33,7 +34,41 @@ module SRNSW
       @this_page.to_s + " of " + @total_pages.to_s
     end
   end 
-
+  
+  module Pages
+    def pages params, page_param=:page, count=25
+      length = self.count
+      page = SRNSW::Utils.numeric_param params[page_param]
+      page = 1 unless page
+      if page_param == :page
+        count = SRNSW::Utils max_param params[page_param], 200, count
+      end  
+      @page_details = SRNSW::Pagination.new(page, count, length, page_param)
+    end 
+    
+    def page_details
+      @page_details
+    end
+  end
+  
+  module Utils
+    module_function
+    def numeric_param page
+      if page
+        page = page.to_i > 0 ? page.to_i : nil
+      end
+    end
+   
+    def max_param param, max, default=nil
+      param = numeric_param(param)
+      if param
+         param > max ? max : param
+      else
+        default
+      end
+    end
+  end
+  
   def pages model, default=25, page_param=:page
     length = model.count
     page = numeric_param page_param
@@ -65,13 +100,13 @@ module SRNSW
     builder.instruct!
     root = set.first.class.name.pluralize.downcase
     builder.tag!(root, {:type => "array"}) do |xml|
-      set.to_a.unshift(@page_details).each do |record|
+      set.to_a.unshift(set.page_details).each do |record|
       xml << record.to_xml(:skip_instruct => true)
       end
     end
   end
   
   def to_paginated_json set
-    set.to_a.unshift(@page_details).to_json
+    set.to_a.unshift(set.page_details).to_json
   end
 end
