caba0d71b90882d2b4c53db9f19658174acdf5a9
richard-srnsw <richard.lehane@records.nsw.gov.au>
2011-07-07 16:42:07 +1000

    fixing up eac-cpf

--- app/helpers/eac_cpf_helper.rb
+++ app/helpers/eac_cpf_helper.rb
@@ -1,11 +1,18 @@
 module EacCpfHelper
   module_function
   def to_eac_formatting field
-    field = field.html_safe.gsub(/\n/, '</p><p>')
     field = '<p>' + field + '</p>'
-    field.gsub!(/<p><\/p>/, '')
-    field.gsub!(/<i>/, '<span style="font-style:italic">').gsub!(/<\/i>/, '</span>')
-    field.gsub!(/<em>/, '<span style="font-style:italic">').gsub!(/<\/em>/, '</span>')
-    field.gsub!(/<b>/, '<span style="font-weight:bold">').gsub!(/<\/b>/, '</span>')
+    field.gsub!(/\n/, '</p><p>')
+    field.gsub!(/<br>/, '</p><p>')
+    field.gsub!(/<br\s*\/>/, '</p><p>')  
+    field.gsub!(/<p>\s*<\/p>/, '')
+    field.gsub!(/<i>/, '<span style="font-style:italic">')
+    field.gsub!(/<\/i>/, '</span>')
+    field.gsub!(/<em>/, '<span style="font-style:italic">')
+    field.gsub!(/<\/em>/, '</span>')
+    field.gsub!(/<b>/, '<span style="font-weight:bold">')
+    field.gsub!(/<\/b>/, '</span>')
+    field.gsub!(/&/, '&amp;')
+    field.html_safe
   end
 end
