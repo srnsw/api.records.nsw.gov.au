599712db75c72697cc137878751282986d70f61a
richard-srnsw <richard.lehane@records.nsw.gov.au>
2011-07-08 16:57:19 +1000

    eac regexes to fix markup

--- app/helpers/eac_cpf_helper.rb
+++ app/helpers/eac_cpf_helper.rb
@@ -3,18 +3,21 @@ module EacCpfHelper
   def to_eac_formatting field
     field = '<p>' + field + '</p>'
     field.gsub!(/\n/, '</p><p>')
-    field.gsub!(/<a.*?>/, '')
-    field.gsub!(/<\/a>/, '')
-    field.gsub!(/<br>/, '</p><p>')
-    field.gsub!(/<br\s*\/>/, '</p><p>')  
+    field.gsub!(/<br\s*\/?>/, '</p><p>')  
+    field.gsub!(/&nbsp;/, ' ')
+    field.gsub!(/&(middot|ndash|emdash);/, '-')
+    field.gsub!(/&quot;/, '"')
+    field.gsub!(/&hellip;/, '...')
+    field.gsub!(/&[lr][sd]quo;/, "'")
     field.gsub!(/<p>\s*<\/p>/, '')
+    field.gsub!(/<\/?(?!(em|[\/pbi])).*?>/, '') # ... HErE bE DRAgONs [should strip any html except - p, em, b, i tags. Maybe requiring nokogiri/loofah would be saner!]
     field.gsub!(/<i>/, '<span style="font-style:italic">')
     field.gsub!(/<\/i>/, '</span>')
     field.gsub!(/<em>/, '<span style="font-style:italic">')
     field.gsub!(/<\/em>/, '</span>')
     field.gsub!(/<b>/, '<span style="font-weight:bold">')
     field.gsub!(/<\/b>/, '</span>')
-    field.gsub!(/&/, '&amp;')
+    field.gsub!(/&(?!amp;)/, '&amp;')
     field.html_safe
   end
 end
