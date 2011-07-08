7d500f8745b8fcfe351a418544a5b3e9459bfbb8
richard-srnsw <richard.lehane@records.nsw.gov.au>
2011-07-08 11:26:30 +1000

    eac_cpf fixes - now validates

--- app/helpers/eac_cpf_helper.rb
+++ app/helpers/eac_cpf_helper.rb
@@ -3,6 +3,8 @@ module EacCpfHelper
   def to_eac_formatting field
     field = '<p>' + field + '</p>'
     field.gsub!(/\n/, '</p><p>')
+    field.gsub!(/<a.*?>/, '')
+    field.gsub!(/<\/a>/, '')
     field.gsub!(/<br>/, '</p><p>')
     field.gsub!(/<br\s*\/>/, '</p><p>')  
     field.gsub!(/<p>\s*<\/p>/, '')
