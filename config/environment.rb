0e21e648d00db8ef0b40f95b28a812b7f50b80ce
wisanup <airgear01@gmail.com>
2011-08-08 09:34:54 +1000

    Removed invalid code

--- config/environment.rb
+++ config/environment.rb
@@ -4,20 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Load SRNSW module
 require 'srnsw.rb'
 
-#require 'magic_multi_connections'
-connection_names = ActiveRecord::Base.configurations.keys.select do |name|
-name =~ /^#{ENV['RAILS_ENV']}_clone/
-end
-
-@@connection_pool = connection_names.map do |connection_name|
-Object.class_eval <<-EOS
-module #{connection_name.camelize}
-establish_connection :#{connection_name}
-end
-EOS
-connection_name.camelize.constantize
-end
-
 # Initialize the rails application
 Collection::Application.initialize!
 
