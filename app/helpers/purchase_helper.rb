3db2fcd3779b9d93ad064101efb57feead4eb7e3
Wisanu Promthong <airgear01@gmail.com>
2011-10-25 09:19:12 +1200

    Reverted the recent commit

--- app/helpers/purchase_helper.rb
+++ app/helpers/purchase_helper.rb
@@ -5,7 +5,8 @@ module PurchaseHelper
   end
 
   def can_pre_order_copy series
-    [475, 9590, 12462, 13495, 13660, 15051, 19348].include? series
+    not [13278, 15244].include? series
+    #[475, 9590, 12462, 13495, 13660, 15051, 19348].include? series
   end
   
 end
